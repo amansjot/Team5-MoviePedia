7974d6c47b8e6b7f4c71cbb20cdbb797c0f76239
Sakhee Desai <116905001+sakheed@users.noreply.github.com>
2023-04-13 14:33:03 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+//import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
     <React.StrictMode>
