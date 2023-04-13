b5b03325ed8bfecc57eeb94c8ecd4012e485a703
Aman Singh <amansjot@gmail.com>
2023-04-13 16:22:55 -0400

    first changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Center, Container, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
+            <Heading mb={10}>
+                <Center>
+                    <img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/200/external-film-cinema-kiranshastry-lineal-kiranshastry-3.png" alt="logo" />
+                    {/* src={logo} */}
+                </Center>
+                <p>MoviePedia</p>
+            </Heading>
+            <Container>
+                <b>Group Members: </b>
+                Heni Patel
+            </Container>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+const colors = {
+    brand: {
+        900: "#1a365d",
+        800: "#153e75",
+        700: "#2a69ac",
+    },
+};
+
+const theme = extendTheme({ colors });
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <ChakraProvider theme={theme}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
