291acb1c0160c3d8f71aeea72589c0a2811f3ee8
Aman Singh <amansjot@gmail.com>
2023-04-17 19:51:28 -0400

    test edit 3

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 
 export function Movie({name, poster, year, actors, plot, director, genre, rating }:{
     name: string, poster: string, year: number, actors: string[], plot: string, director: string, genre: string, rating: number }
-    ): JSX.Element{
+): JSX.Element{
     return(
-        const [n, setName] = useState<string>("John Doe");
+        <div>
+            
+        </div>
+        // const [n, setName] = useState<string>("John Doe");
     );
 }
