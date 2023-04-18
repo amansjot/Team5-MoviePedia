b2191254f6a2b566317e538e349c5cadea6316ba
Heni <heni@udel.edu>
2023-04-18 14:43:01 -0400

    add

--- src/components/CreateList.tsx
+++ src/components/CreateList.tsx
@@ -1,8 +1,49 @@
 import React, { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { Form } from "react-bootstrap";
 
 export function CreateList(): JSX.Element {
-    return(
-        <div>Create a List</div>
+    const [edit, setEdit] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [student, setStudent] = useState<boolean>(true);
+
+    function updateEdit(event: React.ChangeEvent<HTMLInputElement>) {
+        setEdit(event.target.checked);
+    }
+
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setStudent(event.target.checked);
+    }
+
+    return (
+        <div>
+            <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="edit switch"
+                checked={edit}
+                onChange={updateEdit}
+            />
+
+            <div style={{ display: edit ? "block" : "none" }}>
+                <Form.Label> Name: </Form.Label>
+                <Form.Control value={name} onChange={updateName} />
+                <Form.Check
+                    type="checkbox"
+                    id="edit student status"
+                    label="Is A Student?"
+                    checked={student}
+                    onChange={updateStudent}
+                />
+            </div>
+            <div style={{ display: edit ? "none" : "block" }}>
+                {edit
+                    ? ""
+                    : name + " is " + (student ? "" : "not") + " a student."}
+            </div>
+        </div>
     );
 }
