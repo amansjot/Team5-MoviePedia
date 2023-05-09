75f77616fa9f2d567c859303d2a3eb5b7df0f2d4
Julia ONeill <jmoneill@udel.edu>
2023-05-09 13:38:08 -0400

    added a second box for the specialized list, will need to adjust once ive merged

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -26,28 +26,50 @@ export function DragAndDrop(): JSX.Element {
     return(
         <div id="movie-list" onDrop={handleOnDrop}
             onDragOver={handleDragOver}>
-            <Box borderWidth="3px" borderRadius="lg" bg="gray.600" p={10} w="75%" h="100%">
-                <Heading>
-                    <Text size="md">Main List</Text>
-                </Heading>
-                <SimpleGrid h="500px" w="600px" p="4"  spacing = {5} templateColumns={{base: "repeat(3, 1fr)"}}>   
-                    {movieList.map((movie)=>(
-                        <Card align="center" backgroundColor="gray.300" border="1px solid #aaa" pb={5} maxW="sm" direction={{base: "row", sm:"column"}} overflow="hidden" variant="elevated" key={movie.name}>
-                            <CardHeader key={movie.name}>
-                                <Heading size="sm">
-                                    <Text><span>{movie.name} ({movie.year})</span></Text>
-                                </Heading>
-                                <Text><i>{movie.director}</i></Text>
-                            </CardHeader>
-                            <CardBody mt={-5}>
-                                <Image width={79} src={movie.poster} alt={movie.name}></Image>
-                                <div></div>
-                            </CardBody>
-                        </Card>
-                    ))}
-                    
-                </SimpleGrid>
-            </Box>
+            <SimpleGrid spacing={3} columns={2}>
+                <Box borderWidth="3px" borderRadius="lg" bg="gray.600" p={10} w="100%" h="100%">
+                    <Heading>
+                        <Text size="md">Main List</Text>
+                    </Heading>
+                    <SimpleGrid h="500px" w="600px" p="4"  spacing = {5} templateColumns={{base: "repeat(3, 1fr)"}}>   
+                        {movieList.map((movie)=>(
+                            <Card align="center" backgroundColor="gray.300" border="1px solid #aaa" pb={5} maxW="sm" direction={{base: "row", sm:"column"}} overflow="hidden" variant="elevated" key={movie.name}>
+                                <CardHeader key={movie.name}>
+                                    <Heading size="sm">
+                                        <Text><span>{movie.name} ({movie.year})</span></Text>
+                                    </Heading>
+                                    <Text><i>{movie.director}</i></Text>
+                                </CardHeader>
+                                <CardBody mt={-5}>
+                                    <Image width={79} src={movie.poster} alt={movie.name}></Image>
+                                    <div></div>
+                                </CardBody>
+                            </Card>
+                        ))}
+                    </SimpleGrid>
+                </Box>
+                <Box borderWidth="3px" borderRadius="lg" bg="gray.600" p={10} w="100%" h="100%">
+                    <Heading>
+                        <Text size="md"> List</Text>
+                    </Heading>
+                    <SimpleGrid h="500px" w="600px" p="4"  spacing = {5} templateColumns={{base: "repeat(3, 1fr)"}}>   
+                        {movieList.map((movie)=>(
+                            <Card align="center" backgroundColor="gray.300" border="1px solid #aaa" pb={5} maxW="sm" direction={{base: "row", sm:"column"}} overflow="hidden" variant="elevated" key={movie.name}>
+                                <CardHeader key={movie.name}>
+                                    <Heading size="sm">
+                                        <Text><span>{movie.name} ({movie.year})</span></Text>
+                                    </Heading>
+                                    <Text><i>{movie.director}</i></Text>
+                                </CardHeader>
+                                <CardBody mt={-5}>
+                                    <Image width={79} src={movie.poster} alt={movie.name}></Image>
+                                    <div></div>
+                                </CardBody>
+                            </Card>
+                        ))}
+                    </SimpleGrid>
+                </Box>
+            </SimpleGrid>
         </div>
     );
 }
