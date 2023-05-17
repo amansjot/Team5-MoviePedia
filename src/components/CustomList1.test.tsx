f6b1b0088638b049192b855c6f786629a52f6284
Julia ONeill <jmoneill@udel.edu>
2023-05-17 17:47:59 -0400

    trying to fix a merge will go back to resolve the testing.tsx issue later

---
+++ src/components/CustomList1.test.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { CustomList1 } from "./CustomList1";
+import { Button,  } from "@chakra-ui/react";
+
+/*
+    test( ,()=>{
+
+    });
+*/
+/*
+describe("Custom List 1 Component tests", () => {
+    beforeEach(() => {
+        render(<CustomList1 />);
+    });
+
+    test("The user list is not initially visible", ()=>{
+        const list = screen.getByTestId("userList");
+        expect(list).toBeNull();
+    });
+    test("There is a custom list button",()=>{
+        const listButton = screen.getByRole("button", {name: "First Custom List"});
+        expect(listButton).toBeInTheDocument();
+    });
+
+    test("Testing to see if button reveals the list",()=>{
+        const listButton = screen.getByRole("button", {name: "First Custom List"});
+        listButton.click();
+        const list = screen.getByTestId("userList");
+        expect(list).toBeInTheDocument;
+    });
+
+/*
+    test( ,()=>{
+
+    });
+
+    test( ,()=>{
+
+    });
+*/
+//});
