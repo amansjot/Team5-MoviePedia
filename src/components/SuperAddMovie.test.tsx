ba3bb11bd73756c94d470f24b6a2cc788c01fea8
Julia ONeill <jmoneill@udel.edu>
2023-05-18 22:52:57 -0400

    adding more tests

--- src/components/SuperAddMovie.test.tsx
+++ src/components/SuperAddMovie.test.tsx
@@ -1,1 +1,28 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import { SuperAddMovie } from "./SuperAddMovie";
+
+/*
+    test( ,()=>{
+        render();
+    
+    });
+*/
+
+
+describe("Genre Component tests", () => {
+    test("Testing to see is Super role select is there" ,()=>{
+        render(
+            <SuperAddMovie/>
+        );
+        expect(screen.getByRole("combobox")).toBeInTheDocument();
+    });
+    test("There is a box for edit movie",()=>{
+        render(
+            <SuperAddMovie/>
+        );
+        const movieBox = screen.getByTestId("edit-movie-box");
+        expect(movieBox).toBeInTheDocument(); 
+    });
+    
+});
